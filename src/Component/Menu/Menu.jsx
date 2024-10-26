79b7ef278f2e3d4d1ca57b13240bd85e26ce4482
Eurus <exampl@gamil.com>
2024-10-26 05:08:54 +0300

    firstTest

--- src/Component/Menu/Menu.jsx
+++ src/Component/Menu/Menu.jsx
@@ -2,12 +2,11 @@ import style from "./Menu.module.css";
 /* import Button from "./../Button/Button";
 import { borderType, buttonColor, textColor } from "../Button/variables"; */
 import { Link } from "react-router-dom";
-import MyButton from '../MyButton/MyButton';
+/* import MyButton from "../MyButton/MyButton"; */
 
 const Menu = () => {
   return (
     <header className={style.conteiner}>
-<<<<<<< HEAD
       <div className={style.menu}>
         <div>
           <Link to="">
@@ -30,29 +29,6 @@ const Menu = () => {
           {/* <MyButton>Перейти</MyButton> */}
         </nav>
       </div>
-=======
-      <div>
-        <Link to="">
-          <img src="/src/assets/logo.png" alt="" />
-        </Link>
-      </div>
-      <nav className={style.menuNav}>
-        <ul>
-          <li>
-            <Link to="">Домой</Link>
-          </li>
-          <li>
-            <Link to="About">О нас</Link>
-          </li>
-        </ul>
-        <Button
-          text="Hello"
-          bgColor={buttonColor.poorgray}
-          borderType={borderType.two}
-          textColor={textColor.white}
-        />
-      </nav>
->>>>>>> 644e1ac8b01c7ab4244df96a87cdf34049fcc3c4
     </header>
   );
 };
